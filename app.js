0209508a8f791508289373060f8423a39ab8676c
Dovvahkiin <tovarnicki.b@gmail.com>
2025-06-25 18:44:52 +0200

    Add mongodb local database and edited code for it

--- app.js
+++ app.js
@@ -3,28 +3,48 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+
+//Set up default mongoose connection string localy
+const connectionString = 'mongodb://localhost:27017/BlogPost';
+mongoose.connect(connectionString); //connect and create a new database or use if it exists
+
 
 //declaration of app variables
 const app = express();
 const port = 1250;
 
-const aboutContent = "Dovvahkiin, Greybeards from High Hrothgar are calling you!"
-const contactContent = "If you are smart, you will find the way to contact me!"
-
-    
-
-
 app.set('view engine','ejs'); // setting view engine to embedded javascript (ejs)
-
 app.use(bodyParser.urlencoded({extended:true})); // setting body-parser
 app.use(express.static("public")); // using express static files from folder "public"
 
 
+const aboutContent = "Dovvahkiin, Greybeards from High Hrothgar are calling you!"
+const contactContent = "If you are smart, you will find the way to contact me!"
+
 let posts = [];
 
-app.get('/', function(req, res)
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    postMessage: {
+        type: String,
+        required: true
+    }
+});
+
+const PostModel = mongoose.model("post",postSchema);
+
+
+app.get('/home', async function(req, res)
 {
-    res.render("home", {postovine:posts});
+    const getData = await PostModel.find({});
+
+    res.render("home",{postovine:getData})
+
 });
 
 app.get('/about', function(req, res)
@@ -35,34 +55,52 @@ app.get('/about', function(req, res)
 app.get('/contact', function(req, res)
 {
     res.render("contact", {contactContentEjs:contactContent});
-});
+}); 
 
 app.get('/compose', function(req, res)
 {
     res.render("compose", {});
 });
 
-app.post("/compose", function(req,res)
+
+app.post("/compose", async (req,res)=>
 {
     let naslov = req.body.postTitle;
     let tekst = req.body.postText;
-
-    let blogPostInfo =
-    {
+    const newPost = new PostModel({
         title: naslov,
-        text: tekst
+        postMessage: tekst
+    });
+    try
+    {
+        await newPost.save();
+        res.redirect("/home");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+)
+ 
+app.get("/posts/:id", async function(req,res)
+{
+    postId = req.params.id;
+    
+    try
+    {
+        const getData = await PostModel.findById(postId);
+        res.render("post",{
+            postSeparateTitle:getData.title,
+            postSeparateText:getData.postMessage
+        })
+    }
+    catch(err)
+    {
+        console.log(err)
     }
 
-    posts.push(blogPostInfo);
-
-    res.redirect("/");
-})
-
-app.get("/posts/:postName", function(req,res)
-{
-    parameterName = _.lowerCase(req.params.postName);
-
-    posts.forEach(function(postNumber)
+/*     getData.forEach(function(postNumber)
         {
             postFixedTitle = _.lowerCase(postNumber.title);
             if(postFixedTitle === parameterName)
@@ -73,7 +111,7 @@ app.get("/posts/:postName", function(req,res)
                 });
             }
         }       
-    )
+    ) */
 })
 
 
