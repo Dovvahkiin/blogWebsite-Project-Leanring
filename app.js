adaf4680802f3a08354942c67574aba2389df33c
Dovvahkiin <tovarnicki.b@gmail.com>
2025-06-25 18:58:39 +0200

    removed unnecessary stuff

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const _ = require('lodash');
 const mongoose = require('mongoose');
 
 
@@ -14,16 +13,13 @@ mongoose.connect(connectionString); //connect and create a new database or use i
 //declaration of app variables
 const app = express();
 const port = 1250;
+const aboutContent = "Dovvahkiin, Greybeards from High Hrothgar are calling you!"
+const contactContent = "If you are smart, you will find the way to contact me!"
+
 
 app.set('view engine','ejs'); // setting view engine to embedded javascript (ejs)
 app.use(bodyParser.urlencoded({extended:true})); // setting body-parser
 app.use(express.static("public")); // using express static files from folder "public"
-
-
-const aboutContent = "Dovvahkiin, Greybeards from High Hrothgar are calling you!"
-const contactContent = "If you are smart, you will find the way to contact me!"
-
-let posts = [];
 
 const postSchema = mongoose.Schema({
     title: {
@@ -35,22 +31,24 @@ const postSchema = mongoose.Schema({
         required: true
     }
 });
-
 const PostModel = mongoose.model("post",postSchema);
 
+app.get('/', function(req, res)
+{
+    res.redirect('/home');
+});
 
 app.get('/home', async function(req, res)
 {
     const getData = await PostModel.find({});
-
     res.render("home",{postovine:getData})
-
 });
 
 app.get('/about', function(req, res)
 {
     res.render("about", {aboutContentEjs:aboutContent});
 });
+
 
 app.get('/contact', function(req, res)
 {
@@ -62,11 +60,11 @@ app.get('/compose', function(req, res)
     res.render("compose", {});
 });
 
-
-app.post("/compose", async (req,res)=>
+app.post("/compose", async function(req,res)
 {
     let naslov = req.body.postTitle;
     let tekst = req.body.postText;
+    //new post object based on inputs
     const newPost = new PostModel({
         title: naslov,
         postMessage: tekst
@@ -79,9 +77,7 @@ app.post("/compose", async (req,res)=>
     catch(err){
         console.log(err);
     }
-}
-
-)
+});
  
 app.get("/posts/:id", async function(req,res)
 {
@@ -95,25 +91,10 @@ app.get("/posts/:id", async function(req,res)
             postSeparateText:getData.postMessage
         })
     }
-    catch(err)
-    {
+    catch(err){        
         console.log(err)
     }
-
-/*     getData.forEach(function(postNumber)
-        {
-            postFixedTitle = _.lowerCase(postNumber.title);
-            if(postFixedTitle === parameterName)
-            {
-                res.render("post", {
-                    postSeparateTitle: postNumber.title,
-                    postSeparateText: postNumber.text
-                });
-            }
-        }       
-    ) */
-})
-
+});
 
 app.listen(port, function(req,res)
 {
